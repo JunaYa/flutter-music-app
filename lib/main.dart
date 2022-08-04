5462b897b2bf9f826fdf1cb42a18328a8d9723f5
YajunWang <wangyajun@hetao101.com>
2022-08-04 10:04:06 +0800

    feat: remove page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:app/pages/DribbbleMusicPlayer.dart';
 import 'package:app/pages/Home.dart';
-import 'package:app/pages/ReportReading.dart';
 import 'package:flutter/material.dart';
 
 void main() {
